824ad0439c5d135d55db0f387bde3a1a009ebec6
BK-bokai <bokai830124@gmail.com>
2019-08-17 21:11:32 +0800

    更新menubug 剩下會員管理跟編輯作品

---
+++ qwewqa=admin/member.php
@@ -0,0 +1,113 @@
+<?php
+@session_start();
+require_once "../php/db.php";
+require_once "../php/function.php";
+$data = get_index_photo();
+$student = get_student();
+$student_skills = get_student_skills();
+$work = get_work();
+$work_skills = get_work_skills();
+?>
+<?php
+
+
+if (!isset($_SESSION['login']) || !$_SESSION['login']) {
+  //直接轉跳到 login.php
+  header("Location: ../login.php?msg=請正確登入");
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>BK-Admin</title>
+
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+  <!-- materializecss icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+  <!-- jqury CDN -->
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+  <!-- font-awesom -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
+
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <![endif]-->
+  <!--css樣式-->
+  <link rel="stylesheet" href="../css/style.css" charset="utf-8">
+
+  <link rel="Shortcut Icon" type="image/x-icon" href="img/PBLAP_logo_small_c.png">
+
+
+</head>
+
+<body>
+
+  <header>
+    <?php include_once "menu.php" ?>
+  </header>
+
+
+  <main>
+    <div class="row container">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Item Name</th>
+            <th>Item Price</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+            <td>$0.87</td>
+          </tr>
+          <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+            <td>$3.76</td>
+          </tr>
+          <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+            <td>$7.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+  </main>
+
+
+  <?php include_once "footer.php" ?>
+
+
+
+  <script>
+    $(document).ready(function() {
+      $('.sidenav').sidenav();
+
+
+    });
+  </script>
+
+
+</body>
+
+</html>
